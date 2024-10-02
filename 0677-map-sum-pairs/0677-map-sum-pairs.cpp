2a6e8eeea27996396405ebf3800e5adb865de1d6
Rajat Kumar Dabas <154535670+rkdabas@users.noreply.github.com>
2024-10-02 13:57:27 +0530

    Time: 6 ms (25.97%), Space: 11.2 MB (35.27%) - LeetHub

--- 0677-map-sum-pairs/0677-map-sum-pairs.cpp
+++ 0677-map-sum-pairs/0677-map-sum-pairs.cpp
@@ -1,29 +1,77 @@
-class MapSum {
+// using trie
+struct TrieNode {
 public:
-// brute
-unordered_map<string,int>m;
-    MapSum() {
-        
+    TrieNode* children[26];
+    int sum;
+    bool isEnd;
+    
+    TrieNode() {
+        for (int i = 0; i < 26; i++) {
+            children[i] = NULL;
+        }
+        sum = 0;
+        isEnd = false;
+    }
+};
+
+class Trie {
+public:
+    TrieNode* root;
+    unordered_map<string, int> keyMap; // To store the original values of keys
+    
+    Trie() {
+        root = new TrieNode();
     }
     
+    void insert(string word, int currVal) {
+        int prevVal = 0;
+        // If the key already exists
+        if (keyMap.find(word) != keyMap.end()) {
+            prevVal = keyMap[word];
+        }
+
+        TrieNode* crawler = root;
+        for (int i = 0; i < word.size(); i++) {
+            int ind = word[i] - 'a';
+            if (crawler->children[ind] == NULL) {
+                crawler->children[ind] = new TrieNode();
+            }
+            crawler->sum += (currVal-prevVal); // Update the sum at each node by delta
+            crawler = crawler->children[ind];
+        }
+        crawler->sum+=(currVal-prevVal);
+        keyMap[word]=currVal;
+    }
+
+    int search(string word) {
+        TrieNode* crawler = root;
+        for (int i = 0; i < word.size(); i++) {
+            int ind = word[i] - 'a';
+            if (crawler->children[ind] == NULL) return 0;
+            crawler = crawler->children[ind];
+        }
+        return crawler->sum;
+    }
+};
+
+class MapSum {
+public:
+    Trie trie;
+    
+    MapSum() {}
+    
     void insert(string key, int val) {
-        m[key]=val;
+        trie.insert(key, val);
     }
     
     int sum(string prefix) {
-        int n=prefix.size();
-        int ans=0;
-        for(auto it:m){
-            string s=it.first;
-            if(s.substr(0,n)==prefix) ans+=it.second;
-        }
-        return ans;
+        return trie.search(prefix);
     }
 };
 
 /**
  * Your MapSum object will be instantiated and called as such:
  * MapSum* obj = new MapSum();
- * obj->insert(key,val);
+ * obj->insert(key, val);
  * int param_2 = obj->sum(prefix);
  */
